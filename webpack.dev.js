709445f52d8bd60cb051d464e5e5b553f6471399
Dev-abdullaziz <abdullaziz_m@hotmail.com>
2021-03-12 17:02:23 +0200

    I've done it all ^_^

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,8 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-
+const {CleanWebpackPlugin} = require("clean-webpack-plugin")
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
@@ -19,6 +20,16 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new CleanWebpackPlugin({
+            dry: true,
+            verbose: true,
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+        new BundleAnalyzerPlugin()
+
+
     ]
+
 }
